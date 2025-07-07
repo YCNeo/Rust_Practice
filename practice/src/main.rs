a833f43f854a46bed5562094210be3483fa2b00c
Y.C.Neo <elephantneo01@gmail.com>
2025-07-07 17:11:24 +0800

    tuple and array practice

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
-    let mut x: i32 = 5;
-    println!("The value of x is: {}", x);
-    x += 1; // This line will cause a compile-time error because x is immutable
-    println!("The value of x is now: {}", x);
+    let mut tup = (50, 1.0, "hello", true);
+    println!("Tuple: {:?}", tup);
+    println!("Tuple: {:#?}", tup);
+    println!("Tuple: {tup:#?}");
+    tup.0 = 100; // Change the first element
+    println!("Updated Tuple: {:?}", tup);
+    let (x, y, z, i) = tup; // Ignore the last element
+    println!("x: {}, y: {}, z: {}, i: {i}", x, y, z);
+    
+    let arr = [1, 2, 3, 4, 5];
+    println!("Array: {:?}", arr);
+    println!("Array: {:#?}", arr);
+    println!("First element: {}", arr[0]);
+    println!("Last element: {}", arr[arr.len() - 1]);
+    println!("Array length: {}", arr.len());
+    println!("Array is empty: {}", arr.is_empty());
+    println!("Array contains 3: {}", arr.contains(&3));
 }
